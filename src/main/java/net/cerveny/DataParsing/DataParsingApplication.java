74e069607a83cc0765ee3a6c9c76fe5c11ee57e7
Martin Cerveny <martin@cerveny.net>
2023-10-22 22:39:54 +0200

    Add argument for input

--- src/main/java/net/cerveny/DataParsing/DataParsingApplication.java
+++ src/main/java/net/cerveny/DataParsing/DataParsingApplication.java
@@ -5,16 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class DataParsingApplication implements CommandLineRunner
-{
-
-	public static void main(String[] args)throws Exception{		
+public class DataParsingApplication implements CommandLineRunner {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DataParsingApplication.class, args);
 	}
+
 	public void run(String... args)throws Exception{
-		FileParsing fileParsing = new FileParsing();
-    	fileParsing.ReadInputFile("./../../../test.gpc");
+		for (int i=0;i<args.length; i++) {
+			System.out.println("args[" + i + "]: " + args[i]);
+		}
+
 		
+		if(args.length == 0){
+			System.out.println("Missing file argument.");
+		}else{
+			String filePath = args[0];
+			System.out.println("Reading from file: " + filePath);
+			FileParsing fileParsing = new FileParsing();
+    		fileParsing.ReadInputFile(filePath);
+		}
+		
+    	//fileParsing.ReadInputFile("../../test.gpc");		
 	}
 
 }
