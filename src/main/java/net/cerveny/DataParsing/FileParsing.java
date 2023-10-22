39d80c85f0bbe2997125d77dc180c286fb768b1a
Martin Cerveny <martin@cerveny.net>
2023-10-22 21:15:44 +0200

    Add Database and writing to database

--- src/main/java/net/cerveny/DataParsing/FileParsing.java
+++ src/main/java/net/cerveny/DataParsing/FileParsing.java
@@ -1,7 +1,11 @@
 package net.cerveny.DataParsing;
 
 import java.io.File; // Import the File class
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner; // Import the Scanner class to read text files
+
+import org.bson.Document;
 
 public class FileParsing {
   public void ReadInputFile(String path) throws Exception {
@@ -9,27 +13,37 @@ public class FileParsing {
     if (!inputFile.exists() || (inputFile.length() == 0)) {
       System.out.println("The input file is empty or missing.");
     } else {
-      Scanner myReader = new Scanner(inputFile);
+      List<Document> statementList = new ArrayList<Document>();
+      List<Document> movementList = new ArrayList<Document>();
+      Scanner myReader = new Scanner(inputFile);               
+
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
         switch (data.substring(0, 3)) {
           case "074":
             Statement vypis = new Statement(data);
-            System.out.println("input: \n" + data);
-            System.out.println("output: \n" + vypis);
+            //System.out.println("input: \n" + data);
+            //System.out.println("output: \n" + vypis);           
+            statementList.add(vypis.toDoc());
+            
+
             break;
           case "075":
             Movement platba = new Movement(data);
-            System.out.println("input: \n" + data);
-            System.out.println("output: \n" + platba);
+            //System.out.println("input: \n" + data);
+            //System.out.println("output: \n" + platba);            
+            movementList.add(platba.toDoc());
             break;
           default:
             System.out.println("Unknown line case.");
             break;
         }
       }
+      MongoClientConnection.ConnectStatements(statementList);
+      MongoClientConnection.ConnectMovements(movementList);
       myReader.close();
     }
 
   }
 }
+
---
+++ src/main/java/net/cerveny/DataParsing/MongoClientConnection.java
@@ -0,0 +1,33 @@
+package net.cerveny.DataParsing;
+
+import com.mongodb.*;
+
+import java.util.List;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoClientConnection {
+    static String mongoUri="mongodb://localhost:27017/";
+    public static void ConnectStatements(List<Document> input) throws Exception {                
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClient mongoClient = MongoClients.create(connectionString);    
+        MongoDatabase database = mongoClient.getDatabase("test01");   
+        database.createCollection("statements");
+        MongoCollection<Document> statements = database.getCollection("statements");
+        statements.insertMany(input);
+        mongoClient.close();
+    }
+    public static void ConnectMovements(List<Document> input) throws Exception {                
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClient mongoClient = MongoClients.create(connectionString);    
+        MongoDatabase database = mongoClient.getDatabase("test01");   
+        database.createCollection("movements");
+        MongoCollection<Document> movements = database.getCollection("movements");
+        movements.insertMany(input);
+        mongoClient.close();
+    }
+   
+}
