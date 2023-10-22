67844bb9f36a130c16e74f61483e1c6bfa64be24
Martin Cerveny <martin@cerveny.net>
2023-10-22 17:05:58 +0200

    Cleanup Main

--- src/main/java/net/cerveny/DataParsing/DataParsingApplication.java
+++ src/main/java/net/cerveny/DataParsing/DataParsingApplication.java
@@ -2,15 +2,19 @@ package net.cerveny.DataParsing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class DataParsingApplication {
+public class DataParsingApplication implements CommandLineRunner
+{
 
-	public static void main(String[] args) {
-		//SpringApplication.run(DataParsingApplication.class, args);
-		
+	public static void main(String[] args)throws Exception{
+		SpringApplication.run(DataParsingApplication.class, args);
+	}
+	public void run(String... args)throws Exception{
 		FileParsing fileParsing = new FileParsing();
     	fileParsing.ReadInputFile();
+		
 	}
 
 }
